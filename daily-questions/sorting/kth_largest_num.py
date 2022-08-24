b33b13f9e46675710d0aa1c23d69c25813e868d5
betty arega <bettyarega3@gmail.com>
2022-08-24 23:15:29 +0300

    kthLargestNum leetcode medium qn

---
+++ daily-questions/sorting/kth_largest_num.py
@@ -0,0 +1,5 @@
+class Solution:
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        sortedNums =  sorted(map(int,nums),reverse = True)
+        kthLargest = str(sortedNums[k-1])
+        return kthLargest
