aa388c66a4aec11a178215a877f24de0585078fc
betty arega <bettyarega3@gmail.com>
2022-10-04 22:55:02 +0300

    counting valley easy hackerrank qn

---
+++ daily-questions/math/counting_vallety.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    valleyCount = 0
+    altitude = 0
+    for i in range(steps):
+        if path[i] == 'U':
+            if altitude == -1:
+                valleyCount+=1
+            altitude+=1
+        elif path[i] == 'D':
+            altitude-=1
+    return valleyCount
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
